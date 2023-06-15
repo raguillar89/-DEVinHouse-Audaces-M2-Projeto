8d3d2715cf54d5aebc8645d2be93ee70e5b29803
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-06-15 07:27:33 -0300

    U004 - User Interface

---
+++ LabFashion/LabFashion/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using LabFashion.Models;
+
+namespace LabFashion.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUserById(int IdUser);
+        void CreateUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(User user);
+        Task<bool> SaveAllAsync();
+    }
+}
