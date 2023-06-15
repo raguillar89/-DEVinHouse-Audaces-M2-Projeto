eaa66684c6f9d673c71b343880a438d68d3f78d4
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-06-15 14:20:55 -0300

    P002 - Person DTO

--- LabFashion/LabFashion/DTOs/UserDTO.cs
+++ LabFashion/LabFashion/DTOs/UserDTO.cs
@@ -4,15 +4,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LabFashion.DTOs
 {
-    public class UserDTO
+    public class UserDTO : PersonDTO
     {
         [Required(ErrorMessage = "É necessário inserir o Tipo de Usuário")]
         public TypeUser TypeUser { get; set; }
 
         [Required(ErrorMessage = "É necessário inserir o Status de Usuário")]
         public SystemStatus SystemStatus { get; set; }
-
-        [Required]
-        public Person? Person { get; set; }
     }
 }
