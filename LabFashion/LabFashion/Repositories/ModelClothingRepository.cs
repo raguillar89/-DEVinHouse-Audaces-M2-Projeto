cf207e980dffc93c7100ce0617763bce91f1078f
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-06-16 09:58:32 -0300

    MC005 - ModelClothing Repository

---
+++ LabFashion/LabFashion/Repositories/ModelClothingRepository.cs
@@ -0,0 +1,47 @@
+﻿using LabFashion.Context;
+using LabFashion.Models;
+using LabFashion.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabFashion.Repositories
+{
+    public class ModelClothingRepository : IModelClothingRepository
+    {
+        private readonly LCCContext _lccContext;
+
+        public ModelClothingRepository(LCCContext lccContext)
+        {
+            _lccContext = lccContext;
+        }
+
+        public async Task<IEnumerable<ModelClothing>> GetModelsClothing()
+        {
+            return await _lccContext.Models.Include(x => x.ClothingCollection).ToListAsync();
+        }
+
+        public async Task<ModelClothing> GetModelsClothingById(int IdModel)
+        {
+            return await _lccContext.Models.Where(x => x.IdModel == IdModel).FirstOrDefaultAsync();
+        }
+
+        public void CreateModelsClothing(ModelClothing modelClothing)
+        {
+            _lccContext.Models.Add(modelClothing);
+        }
+
+        public void UpdateModelsClothingn(ModelClothing modelClothing)
+        {
+            _lccContext.Entry(modelClothing).State = EntityState.Modified;
+        }
+
+        public void DeleteModelsClothing(ModelClothing modelClothing)
+        {
+            _lccContext.Models.Remove(modelClothing);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _lccContext.SaveChangesAsync() > 0;
+        }
+    }
+}
