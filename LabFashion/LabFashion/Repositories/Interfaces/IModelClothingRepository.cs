706354ce5fa6e00f4866ebdc52fb3a8b72c0f381
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-06-16 08:54:34 -0300

    MC004 - ModelClothing Interface

---
+++ LabFashion/LabFashion/Repositories/Interfaces/IModelClothingRepository.cs
@@ -0,0 +1,14 @@
+﻿using LabFashion.Models;
+
+namespace LabFashion.Repositories.Interfaces
+{
+    public interface IModelClothingRepository
+    {
+        Task<IEnumerable<ModelClothing>> GetModelsClothing();
+        Task<ModelClothing> GetModelsClothingById(int IdModel);
+        void CreateModelsClothing(ModelClothing modelClothing);
+        void UpdateModelsClothingn(ModelClothing modelClothing);
+        void DeleteModelsClothing(ModelClothing modelClothing);
+        Task<bool> SaveAllAsync();
+    }
+}
